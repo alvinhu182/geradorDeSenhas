cdd5107748f4f34b1cca9ce5f9ef43182b45a360
unknown <alvinhu@hotmail.com>
2023-02-24 23:53:39 -0300

    testando em celulares

--- script/script.js
+++ script/script.js
@@ -13,27 +13,14 @@ function gerarSenha(tamanho) {
     const novaSenha = gerarSenha(tamanhoSenha);
     document.getElementById("senhaGerada").innerHTML = novaSenha;
     
+    
     const COPY = document.querySelector('#copy')
     COPY.addEventListener('click', () => {
         navigator.clipboard.writeText(novaSenha)
         alert('A senha foi copiada com sucesso')
       })
-    //document.getElementById("copy").onclick = function() {
-    //    var text = novaSenha
-    //    console.log(text)
-    //    alert('a Senha foi copiada')
-        
-     
-    //    navigator.clipboard.writeText(text)
-    //    .then(() => {
-    //        console.log('Text copied to clipboard');
-     //   })
-     //   .catch(err => {
-      //      console.error('Error in copying text: ', err);
-     //   });
-        
- // }
-})
- ;
+       
+    
+});
 
 
