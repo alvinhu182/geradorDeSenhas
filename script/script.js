8bdf0f63c7ff998ebec59acb6b62a094fd8199e7
unknown <alvinhu@hotmail.com>
2023-02-25 00:17:57 -0300

    melhorias no codigo

--- script/script.js
+++ script/script.js
@@ -1,26 +1,35 @@
 function gerarSenha(tamanho) {
-    const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
-    let senha = "";
-    for (let i = 0; i < tamanho; i++) {
-      senha += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-    }
-    return senha;
+  const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+  let senha = "";
+  for (let i = 0; i < tamanho; i++) {
+    senha += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
   }
-  const botaoGerarSenha = document.getElementById("gerarSenha");
-  
-  botaoGerarSenha.addEventListener("click", function() {
-    const tamanhoSenha = document.getElementById("tamanhoSenha").value;
-    const novaSenha = gerarSenha(tamanhoSenha);
-    document.getElementById("senhaGerada").innerHTML = novaSenha;
-    
-    
-    const COPY = document.querySelector('#copy')
-    COPY.addEventListener('click', () => {
-        navigator.clipboard.writeText(novaSenha)
-        alert('A senha foi copiada com sucesso')
+  return senha;
+}
+const botaoGerarSenha = document.getElementById("gerarSenha");
+
+botaoGerarSenha.addEventListener("click", function() {
+  const tamanhoSenha = document.getElementById("tamanhoSenha").value;
+  let novaSenha = gerarSenha(tamanhoSenha);
+   let pass =  document.getElementById("senhaGerada");
+   pass.innerHTML = novaSenha
+  document.getElementById("copy").onclick = function() {
+      var text = novaSenha
+      console.log(text)
+   
+      if(novaSenha != '') {
+      navigator.clipboard.writeText(text)
+      .then(() => {
+          console.log('Text copied to clipboard');
+          alert('senha copiada')
+          pass.innerHTML = '' 
+          novaSenha = ''
+         
       })
-       
-    
-});
-
-
+      .catch(err => {
+          console.error('Error in copying text: ', err);
+      });
+  } else {
+      alert('Voce não gerou uma senha')
+  }
+}});
